dd63e7ff7566654ca8f3ca6eba49277f01dc55b6
anuragjatofficial <anurag.1101140@gmail.com>
2024-03-27 00:38:07 +0530

    centered all content

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import TopSectorComponent from './components/TopSectorComponent';
 
 function App() {
   return (
-    <div className="App w-[100vw] h-[100vh]  flex bg-[#f3f3f3f3] gap-7">
+    <div className="App w-[100vw] h-[100vh]  flex bg-[#f3f3f3f3] gap-7 justify-center items-center">
       <BaseComponent/>
       <SubCategory/>
       <FundContribution/>
